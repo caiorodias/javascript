e63ca2146899ad58b7f971052f26c9f09c3e6ec3
caiorodias <caiorodias@hotmail.com>
2022-10-23 20:06:52 -0300

    iniciando repetição

---
+++ aula013/ambiente.js
@@ -0,0 +1,17 @@
+let contador = 1
+do{
+    console.log(`Passo ${contador}`)
+    contador++ //igual a contador = contador + 1
+} while (contador <= 6)
+
+
+/* let contador = 1
+while (contador <= 10) {
+    console.log(`Passo ${contador}`)
+    contador++ //igual a contador = contador + 1
+}
+
+- COM WHILE ELE TESTA ANTES DE EXECUTAR
+- COM DO .. WHILE, ELE EXECUTA DEPOIS TESTA
+
+*/
