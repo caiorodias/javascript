e1a238674821099dcfae8b14499dc70bb212fd2a
caiorodias <caiorodias@hotmail.com>
2022-10-23 21:15:34 -0300

    iniciando exercícios

---
+++ aula014/ambiente.js
@@ -0,0 +1,24 @@
+/* let c = 1 */
+
+/* DO .. WHILE 
+do {
+    console.log (c)
+    c++    
+} while (c <=5) */
+
+/* WHILE
+while (c <= 5) {
+    console.log (c)
+    c++
+} */
+
+/* FOR (MELHOR FORMA DE FAZER) */
+/* for (let c = 1; c <= 7; c++) {
+    console.log (c)
+} */
+
+console.log ('Vai começar...')
+for (let c = 1; c <= 3; c++) {
+    console.log (c)
+}
+console.log ('FIM!')
