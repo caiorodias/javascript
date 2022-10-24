85b4b8fcf9359aaad372ce7d4607766db33315c8
caiorodias <caiorodias@hotmail.com>
2022-10-24 11:56:29 -0300

    continunando js

---
+++ exercícios/javascript/scripts3.js
@@ -0,0 +1,14 @@
+function contar() {
+    let inicio = document.querySelector('input#inicio')
+    let fim = document.querySelector('input#fim')
+    let intervalo = document.querySelector('input#intervalo')
+    let soma = []
+    let res = document.querySelector('div#msg')
+
+    while (soma.value <= fim.value) {
+        let resultado = inicio + intervalo
+        
+    }
+    resultado.innerHTML =
+
+}
