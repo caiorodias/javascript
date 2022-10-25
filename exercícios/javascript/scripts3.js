175e9628b32d84ce300f787a7dfdfb479afc3530
caiorodias <caiorodias@hotmail.com>
2022-10-24 22:37:15 -0300

    finalizando contagem

--- exercícios/javascript/scripts3.js
+++ exercícios/javascript/scripts3.js
@@ -1,14 +1,50 @@
 function contar() {
-    let inicio = document.querySelector('input#inicio')
-    let fim = document.querySelector('input#fim')
-    let intervalo = document.querySelector('input#intervalo')
-    let soma = []
-    let res = document.querySelector('div#msg')
+    let x = document.querySelector('input#inicio')
+    let z = document.querySelector('input#fim')
+    let y = document.querySelector('input#intervalo')
+    const res = document.querySelector('div#msg')
+    
 
-    while (soma.value <= fim.value) {
-        let resultado = inicio + intervalo
-        
+    if (x.value.length == 0 || z.value.length == 0 || y.value.length == 0) {
+        window.alert ('[ERRO] Ausência de dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let int = Number(intervalo.value)
+        /* if (p <= 0) {
+            window.alert ('Passo inválido! Considerando intervalo 1')
+            int = 1
+        } */
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c+=int) {
+                res.innerHTML += ` ${c} &#10140;`
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c-= int) {
+                res.innerHTML += ` ${c} &#10140;`
+            }
+        }
+        res.innerHTML += `&#127937;`
     }
-    resultado.innerHTML =
 
+    
 }
+
+/* 
+x = inicio
+y = intervalo
+z = fim
+
+soma = x+y
+
+while (soma <= fim) {
+    soma + y
+}
+
+for (let soma = x+y; soma.value < z.value; soma.value + y.value) {
+            res.innerHTML = `${soma.}`
+        }
+*/
